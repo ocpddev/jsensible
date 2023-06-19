b3865168b50ec627cbc14808aab17afd15a5cb7b
Xin Chen <2467368037@qq.com>
2023-06-19 18:11:18 +0800

    fix: modify `noPathsGet()` rule

--- src/main/kotlin/dev/ocpd/jsensible/rules/java/Java11Rules.kt
+++ src/main/kotlin/dev/ocpd/jsensible/rules/java/Java11Rules.kt
@@ -2,6 +2,7 @@ package dev.ocpd.jsensible.rules.java
 
 import com.tngtech.archunit.lang.ArchRule
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
+import java.net.URI
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.*
@@ -38,7 +39,9 @@ object Java11Rules {
      * Solution: Replace [Paths.get] with [Path.of].
      */
     fun noPathsGet(): ArchRule =
-        noClasses().should().callMethod(Paths::class.java, "get")
+        noClasses()
+            .should().callMethod(Paths::class.java, "get", String::class.java, Array<String>::class.java)
+            .orShould().callMethod(Paths::class.java, "get", URI::class.java)
             .`as`("call [Paths.get]")
             .because("JDK recommends to obtain a [Path] via the [Path.of] methods instead")
 }
