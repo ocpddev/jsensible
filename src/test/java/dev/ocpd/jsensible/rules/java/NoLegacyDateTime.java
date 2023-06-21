8d7c46e77720925f9b7cb5aa10cce78c5cc45329
Xin Chen <2467368037@qq.com>
2023-06-21 10:09:53 +0800

    test: add `noLegacyDateTime()` rule test case and polish import

--- src/test/java/dev/ocpd/jsensible/rules/java/NoLegacyDateTime.java
+++ src/test/java/dev/ocpd/jsensible/rules/java/NoLegacyDateTime.java
@@ -1,9 +1,11 @@
 package dev.ocpd.jsensible.rules.java;
 
+import java.text.DateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 @SuppressWarnings("unused")
 public class NoLegacyDateTime {
@@ -21,15 +23,29 @@ public class NoLegacyDateTime {
 
     public static class NonCompliant1 {
 
-        public java.util.Date nonCompliant() {
+        public Date nonCompliant() {
             return new Date();
         }
     }
 
     public static class NonCompliant2 {
 
-        public java.util.Calendar nonCompliant() {
+        public Calendar nonCompliant() {
             return Calendar.getInstance();
+        }
+    }
+
+    public static class NonCompliant3 {
+
+        public DateFormat nonCompliant() {
+            return DateFormat.getInstance();
+        }
+    }
+
+    public static class NonCompliant4 {
+
+        public TimeZone nonCompliant() {
+            return TimeZone.getDefault();
         }
     }
 }
