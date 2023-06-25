152002afd5395e10762c29b1b94d8d255fc9fcb6
Xin Chen <2467368037@qq.com>
2023-06-25 14:50:37 +0800

    fix: polish description and remove `assignableTo()`

--- src/main/kotlin/dev/ocpd/jsensible/internal/spring/NotSpringNullable.kt
+++ src/main/kotlin/dev/ocpd/jsensible/internal/spring/NotSpringNullable.kt
@@ -2,7 +2,6 @@ package dev.ocpd.jsensible.internal.spring
 
 import com.tngtech.archunit.base.DescribedPredicate.not
 import com.tngtech.archunit.core.domain.JavaClass
-import com.tngtech.archunit.core.domain.JavaClass.Predicates.assignableTo
 import com.tngtech.archunit.lang.ArchCondition
 import com.tngtech.archunit.lang.conditions.ArchConditions.dependOnClassesThat
 import dev.ocpd.jsensible.internal.nullability.NullabilityAnnotations.nullableAnnotations
@@ -18,9 +17,7 @@ internal object NotSpringNullable {
      */
     internal fun notUseSpringNullable(): ArchCondition<JavaClass> =
         dependOnClassesThat(
-            assignableTo(
-                nullableAnnotations()
-                    .and(not(springNullableAnnotation()))
-            )
+            nullableAnnotations()
+                .and(not(springNullableAnnotation()))
         ).`as`("not use spring nullable")
 }
