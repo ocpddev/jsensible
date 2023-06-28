b41452e949e5f6a9f4c7d687a58af53a153d8e51
Xin Chen <2467368037@qq.com>
2023-06-28 10:16:39 +0800

    fix: placeholder instead of entity class

--- src/test/java/dev/ocpd/jsensible/rules/spring/NoNullableMisuseInRepositories.java
+++ src/test/java/dev/ocpd/jsensible/rules/spring/NoNullableMisuseInRepositories.java
@@ -1,102 +1,31 @@
 package dev.ocpd.jsensible.rules.spring;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @SuppressWarnings("unused")
 public class NoNullableMisuseInRepositories {
 
-    @Entity
-    public static class EntityCompliant {
-
-        @Id
-        private Long id;
-
-        private String test1;
-
-        private String test2;
-
-        public Long getId() {
-            return id;
-        }
-
-        public String getTest1() {
-            return test1;
-        }
-
-        public String getTest2() {
-            return test2;
-        }
-    }
-
-    public interface CompliantRepo extends JpaRepository<EntityCompliant, Long> {
+    public interface CompliantRepo extends JpaRepository<Object, Long> {
 
         @org.springframework.lang.Nullable
-        EntityCompliant findByTest1(String test1);
+        Object findBy(String test);
 
-        EntityCompliant findByTest2(@org.springframework.lang.Nullable String test2);
+        Object findObjectBy(@org.springframework.lang.Nullable String test);
     }
 
-    @Entity
-    public static class EntityNonCompliant1 {
-
-        @Id
-        private Long id;
-
-        private String test1;
-
-        private String test2;
-
-        public Long getId() {
-            return id;
-        }
-
-        public String getTest1() {
-            return test1;
-        }
-
-        public String getTest2() {
-            return test2;
-        }
-    }
-
-    public interface NonCompliantRepo1 extends JpaRepository<EntityNonCompliant1, Long> {
+    public interface NonCompliantRepo1 extends JpaRepository<Object, Long> {
 
         @jakarta.annotation.Nullable
-        EntityNonCompliant1 findByTest1(String test1);
+        Object findBy(String test);
 
-        EntityNonCompliant1 findByTest2(@jakarta.annotation.Nullable String test2);
+        Object findObjectBy(@jakarta.annotation.Nullable String test);
     }
 
-    @Entity
-    public static class EntityNonCompliant2 {
-
-        @Id
-        private Long id;
-
-        private String test1;
-
-        private String test2;
-
-        public Long getId() {
-            return id;
-        }
-
-        public String getTest1() {
-            return test1;
-        }
-
-        public String getTest2() {
-            return test2;
-        }
-    }
-
-    public interface NonCompliantRepo2 extends JpaRepository<EntityNonCompliant2, Long> {
+    public interface NonCompliantRepo2 extends JpaRepository<Object, Long> {
 
         @org.jetbrains.annotations.Nullable
-        EntityNonCompliant2 findByTest1(String test1);
+        Object findBy(String test);
 
-        EntityNonCompliant2 findByTest2(@org.jetbrains.annotations.Nullable String test2);
+        Object findObjectBy(@org.jetbrains.annotations.Nullable String test);
     }
 }
