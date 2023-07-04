a06696b710b64674e14727fc157bc9d7e0703c4d
Xin Chen <2467368037@qq.com>
2023-07-04 16:46:03 +0800

    feat: add `check()` to check jsensible rules

--- src/main/kotlin/dev/ocpd/jsensible/JSensible.kt
+++ src/main/kotlin/dev/ocpd/jsensible/JSensible.kt
@@ -1,10 +1,23 @@
 package dev.ocpd.jsensible
 
-import com.tngtech.archunit.lang.CompositeArchRule
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.lang.FailureReport
 import dev.ocpd.jsensible.rules.JSensibleRules
 
 object JSensible {
-    fun all() = CompositeArchRule.of(
-        JSensibleRules.all()
-    ).`as`("JSensible Rules")
+
+    /**
+     * JSensible Rules
+     */
+    fun check(javaClasses: JavaClasses) {
+        val toList = JSensibleRules.all()
+            .map {
+                // When checking, it is possible that no classes have been passed to the rule at all, or that no classes
+                // passed to the rule matched the `that()` clause. So we should use `ArchRule.allowEmptyShould(true)`.
+                it.allowEmptyShould(true).evaluate(javaClasses).failureReport
+            }
+            .filterNot(FailureReport::isEmpty)
+            .toList()
+        if (toList.isNotEmpty()) throw AssertionError(toList.joinToString(separator = "\n\n", postfix = "\n"))
+    }
 }
